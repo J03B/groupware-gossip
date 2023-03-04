ac833282c850aafd9db62f9c7c9777d706473b1a
J03B <byucrazyfan@gmail.com>
2023-03-03 22:34:54 -0600

    Added SQL model setup, configured base routing, included internal API call setup

---
+++ controllers/home-routes.js
@@ -0,0 +1,79 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../models');
+
+// GET all posts for the homepage
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: [ 'username' ],
+        },
+      ],
+    });
+
+    const posts = postData.map((post) =>
+      post.get({ plain: true })
+    );
+    const loggedIn = req.session.loggedIn;
+    res.render('homepage', {
+      posts,
+      loggedIn
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one gallery
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: [
+            'username',
+          ],
+        },
+        { model: Comment, },
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+    const loggedIn = req.session.loggedIn;
+    res.render('post', { post, loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one dashboard
+router.get('/user/:id', async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.params.id);
+
+    const user = userData.get({ plain: true });
+    const loggedIn = req.session.loggedIn;
+    res.render('dashboard', { user, loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Login route
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect to the homepage
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  // Otherwise, render the 'login' template
+  res.render('login');
+});
+
+module.exports = router;
